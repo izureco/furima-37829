7486d69fc1e5d21acbc2cf64e6b0a0f5127810a0
izureco <freestyleaerial@gmail.com>
2022-04-19 12:28:36 +0900

    商品編集機能Rev.2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: :edit
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -20,7 +21,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def destroy
@@ -34,11 +34,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -59,5 +57,9 @@ class ItemsController < ApplicationController
         redirect_to action: :index
       end
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
