82ee83b103e282caab5974ae5d682df06cc97de9
izureco <freestyleaerial@gmail.com>
2022-04-22 15:03:09 +0900

    viewの微調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,6 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: "には英字と数字の両方を含んでください", unless: -> {password.blank?}
 
-  has_many :items
-  has_many :purchase_histories
+  has_many :items, dependent: :destroy
+  has_many :purchase_histories, dependent: :destroy
 end
